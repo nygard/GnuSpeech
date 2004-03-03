b6c37ae83b74e27964dd90c412687f553fee78f2
Steve Nygard <nygard@gmail.com>
2004-03-03 05:30:09 +0000

    PointInspector converted.  84 warnings.

--- Applications/Monet/PointInspector.h
+++ Applications/Monet/PointInspector.h
@@ -1,6 +1,8 @@
 #import <Foundation/NSObject.h>
 #import <AppKit/NSNibDeclarations.h> // For IBAction, IBOutlet
 
+@class GSMPoint;
+
 /*===========================================================================
 
 	Author: Craig-Richard Taube-Schock
@@ -23,45 +25,43 @@ History:
 
 @interface PointInspector:NSObject
 {
-	id	mainInspector;
-	id	popUpListView;
-	id	popUpList;
+    id mainInspector;
+    id popUpListView;
+    id popUpList;
 
-	id	multipleListView;
+    id multipleListView;
 
-	id	valueBox;
-	id	valueField;
-	id	phantomSwitch;
+    id valueBox;
+    id valueField;
+    id phantomSwitch;
 
-	id	type1Button;
-	id	type2Button;
-	id	type3Button;
+    id type1Button;
+    id type2Button;
+    id type3Button;
 
-	id	expressionBrowser;
-	id	currentTimingField;
+    id expressionBrowser;
+    id currentTimingField;
 
-	Point	*currentPoint;
-
+    GSMPoint *currentPoint;
 }
 
-- init;
 - (void)applicationDidFinishLaunching:(NSNotification *)notification;
 
 - (void)inspectPoint:point;
-- (void)setUpWindow:sender;
+- (void)setUpWindow:(id)sender;
 - (void)beginEditting;
 
-- (void)browserHit:sender;
-- (void)browserDoubleHit:sender;
+- (void)browserHit:(id)sender;
+- (void)browserDoubleHit:(id)sender;
 
 - (int)browser:(NSBrowser *)sender numberOfRowsInColumn:(int)column;
 - (void)browser:(NSBrowser *)sender willDisplayCell:(id)cell atRow:(int)row column:(int)column;
 
-- (void)setValue:sender;
-- (void)setType1:sender;
-- (void)setType2:sender;
-- (void)setType3:sender;
+- (void)setValue:(id)sender;
+- (void)setType1:(id)sender;
+- (void)setType2:(id)sender;
+- (void)setType3:(id)sender;
 
-- (void)setPhantom:sender;
+- (void)setPhantom:(id)sender;
 
 @end
--- Applications/Monet/PointInspector.m
+++ Applications/Monet/PointInspector.m
@@ -1,259 +1,221 @@
-
 #import "PointInspector.h"
+
+#import <AppKit/AppKit.h>
 #import "Inspector.h"
-#import "ProtoTemplate.h"
-#import "PrototypeManager.h"
-#import "ProtoEquation.h"
 #import "MyController.h"
-#import <AppKit/NSText.h>
-#import <AppKit/NSApplication.h>
-#import <string.h>
+#import "Point.h"
+#import "ProtoEquation.h"
+#import "PrototypeManager.h"
+#import "ProtoTemplate.h"
 
 @implementation PointInspector
 
-- init
-{
-	return self;
-}
-
-- (void)applicationDidFinishLaunching:(NSNotification *)notification
+- (void)applicationDidFinishLaunching:(NSNotification *)notification;
 {
     [expressionBrowser setTarget:self];
-	[expressionBrowser setAction:(SEL)(@selector(browserHit:))];
-	[expressionBrowser setDoubleAction:(SEL)(@selector(browserDoubleHit:))];
+    [expressionBrowser setAction:@selector(browserHit:)];
+    [expressionBrowser setDoubleAction:@selector(browserDoubleHit:)];
 }
 
-- (void)inspectPoint:point
+- (void)inspectPoint:point;
 {
-	/* Hack for Single Point Inspections */
-	if ([point isKindOfClassNamed:"Point"])
-	{
-		currentPoint = point;
-		[mainInspector setPopUpListView:popUpListView];
-		[self setUpWindow:popUpList];
-	}
-	else
-	if ([point count] == 1)
-	{
-		currentPoint = [point objectAtIndex: 0];
-		[mainInspector setPopUpListView:popUpListView];
-		[self setUpWindow:popUpList];
-	}
-	else
-	{
-		[mainInspector cleanInspectorWindow];
-		[mainInspector setGeneralView:multipleListView];
-
-	} 
+    /* Hack for Single Point Inspections */
+    if ([point isKindOfClass:[GSMPoint class]]) {
+        currentPoint = point;
+        [mainInspector setPopUpListView:popUpListView];
+        [self setUpWindow:popUpList];
+    } else if ([point count] == 1) {
+        currentPoint = [point objectAtIndex: 0];
+        [mainInspector setPopUpListView:popUpListView];
+        [self setUpWindow:popUpList];
+    } else {
+        [mainInspector cleanInspectorWindow];
+        [mainInspector setGeneralView:multipleListView];
+    }
 }
 
-- (void)setUpWindow:sender
+- (void)setUpWindow:(id)sender;
 {
-const char *temp;
-id tempProto = NXGetNamedObject(@"prototypeManager", NSApp);
-id tempCell;
-int index1, index2;
-char buffer[256];
+    NSString *str;
+    id tempProto = NXGetNamedObject(@"prototypeManager", NSApp);
+    id tempCell;
+    int index1, index2;
 
+    str = [[sender selectedCell] title];
+    if ([str hasPrefix:@"C"]) {
+        /* Comment Window */
+#if 0
+        [mainInspector setGeneralView: commentView];
 
-	temp = [[[sender selectedCell] title] cString];
-	switch(temp[0])
-	{
-		/* Comment Window */
-		case 'C':
-/*			[mainInspector setGeneralView: commentView];
+        [setCommentButton setTarget:self];
+        [setCommentButton setAction:@selector(setComment:)];
 
-			[setCommentButton setTarget:self];
-			[setCommentButton setAction:(SEL)(@selector(setComment:))];
+        [revertCommentButton setTarget:self];
+        [revertCommentButton setAction:@selector(revertComment:)];
 
-			[revertCommentButton setTarget:self];
-			[revertCommentButton setAction:(SEL)(@selector(revertComment:))];
+        [commentText setText:[currentParameter comment]];
+#endif
+    } else if ([str hasPrefix:@"G"]) {
+        NSString *path;
 
-			[commentText setText:[currentParameter comment]];*/
+        [mainInspector setGeneralView:valueBox];
+        [expressionBrowser loadColumnZero];
 
-			break;
-		case 'G':
-			[mainInspector setGeneralView:valueBox];
-			[expressionBrowser loadColumnZero];
+        [valueField setDoubleValue:[currentPoint value]];
+        switch ([currentPoint type]) {
+          case DIPHONE:
+              [type1Button setState:1];
+              [type2Button setState:0];
+              [type3Button setState:0];
+              break;
+          case TRIPHONE:
+              [type1Button setState:0];
+              [type2Button setState:1];
+              [type3Button setState:0];
+              break;
+          case TETRAPHONE:
+              [type1Button setState:0];
+              [type2Button setState:0];
+              [type3Button setState:1];
+              break;
+        }
 
-			[valueField setDoubleValue:[currentPoint value]];
-			switch([currentPoint type])
-			{
-				case DIPHONE:
-					[type1Button setState:1];
-					[type2Button setState:0];
-					[type3Button setState:0];
-					break;
-				case TRIPHONE:
-					[type1Button setState:0];
-					[type2Button setState:1];
-					[type3Button setState:0];
-					break;
-				case TETRAPHONE:
-					[type1Button setState:0];
-					[type2Button setState:0];
-					[type3Button setState:1];
-					break;
-			}
+        [phantomSwitch setState:[currentPoint phantom]];
 
-			[phantomSwitch setState:[currentPoint phantom]];
+        tempCell = [currentPoint expression];
+        if (tempCell) {
+            [currentTimingField setStringValue:[[tempCell expression] expressionString]];
+        } else {
+            [currentTimingField setStringValue:[NSString stringWithFormat:@"Fixed: %.3f ms", [currentPoint freeTime]]];
+        }
+        [tempProto findList:&index1 andIndex:&index2 ofEquation:tempCell];
 
-			bzero(buffer,256);
-			tempCell = [currentPoint expression];
-			if (tempCell)
-			{
-				[[tempCell expression] expressionString:buffer];
-				[currentTimingField setStringValue:[NSString stringWithCString:buffer]];
-			}
-			else
-			{
-				sprintf(buffer, "Fixed: %.3f ms", [currentPoint freeTime]);
-				[currentTimingField setStringValue:[NSString stringWithCString:buffer]];
-			}
-			[tempProto findList: &index1 andIndex: &index2 ofEquation: tempCell];
-			sprintf(buffer, "/%s/%s", [[(ProtoEquation *)[[tempProto equationList] objectAtIndex:index1] name] cString],
-				[[(ProtoEquation *)[[[tempProto equationList] objectAtIndex:index1] objectAtIndex: index2] name] cString]);
-			printf("Path = |%s|\n", buffer);
-			[expressionBrowser setPath:[NSString stringWithCString:buffer]];
-			break;
-	} 
+        path = [NSString stringWithFormat:@"/%@/%@",
+                         [(ProtoEquation *)[[tempProto equationList] objectAtIndex:index1] name],
+                         [(ProtoEquation *)[[[tempProto equationList] objectAtIndex:index1] objectAtIndex:index2] name]];
+        NSLog(@"Path = |%@|", path);
+        [expressionBrowser setPath:path];
+    }
 }
 
-- (void)beginEditting
+- (void)beginEditting;
 {
-const char *temp;
+    NSString *str;
 
-	temp = [[[popUpList selectedCell] title] cString];
-	switch(temp[0])
-	{
-		/* Comment Window */
-		case 'C':
-//			[commentText selectAll:self];
-			break;
-
-		case 'D':
-			[valueField selectText: self];
-
-			break;
-	} 
+    str = [[popUpList selectedCell] title];
+    if ([str hasPrefix:@"C"]) {
+        //[commentText selectAll:self];
+    } else if ([str hasPrefix:@"D"]) {
+        [valueField selectText: self];
+    }
 }
 
-- (void)browserHit:sender
+- (void)browserHit:(id)sender;
 {
-int listIndex, index;
-id tempProto = NXGetNamedObject(@"prototypeManager", NSApp);
-id temp;
-char buffer[256];
+    int listIndex, index;
+    id tempProto = NXGetNamedObject(@"prototypeManager", NSApp);
+    id temp;
 
-	if ([sender selectedColumn] == 1)
-	{
-		listIndex = [[sender matrixInColumn:0] selectedRow];
-		index = [[sender matrixInColumn:1] selectedRow];
-		[tempProto findEquation:listIndex andIndex:    index];
-		temp = tempProto;
-		[currentPoint setExpression:temp];
+    if ([sender selectedColumn] == 1) {
+        listIndex = [[sender matrixInColumn:0] selectedRow];
+        index = [[sender matrixInColumn:1] selectedRow];
+        [tempProto findEquation:listIndex andIndex:index];
+        temp = tempProto;
+        [currentPoint setExpression:temp];
 
-		bzero(buffer,256);
-		[[temp expression] expressionString:buffer];
-		[currentTimingField setStringValue:[NSString stringWithCString:buffer]];
+        [currentTimingField setStringValue:[[temp expression] expressionString]];
 
-		[NXGetNamedObject(@"transitionBuilder", NSApp) display];
-		[NXGetNamedObject(@"specialTransitionBuilder", NSApp) display];
+        [NXGetNamedObject(@"transitionBuilder", NSApp) display];
+        [NXGetNamedObject(@"specialTransitionBuilder", NSApp) display];
 
-	} 
+    }
 }
 
-- (void)browserDoubleHit:sender
+- (void)browserDoubleHit:(id)sender;
 {
-	 
 }
 
-- (int)browser:(NSBrowser *)sender numberOfRowsInColumn:(int)column
+- (int)browser:(NSBrowser *)sender numberOfRowsInColumn:(int)column;
 {
-int index;
+    int index;
 
-	if (column == 0)
-		return [[NXGetNamedObject(@"prototypeManager", NSApp) equationList] count];
-	else
-	{
-		index = [[sender matrixInColumn:0] selectedRow];
-		return [[[NXGetNamedObject(@"prototypeManager", NSApp) equationList] objectAtIndex: index] count];
-	}
+    if (column == 0)
+        return [[NXGetNamedObject(@"prototypeManager", NSApp) equationList] count];
+
+    index = [[sender matrixInColumn:0] selectedRow];
+    return [[[NXGetNamedObject(@"prototypeManager", NSApp) equationList] objectAtIndex:index] count];
 }
 
-- (void)browser:(NSBrowser *)sender willDisplayCell:(id)cell atRow:(int)row column:(int)column
+- (void)browser:(NSBrowser *)sender willDisplayCell:(id)cell atRow:(int)row column:(int)column;
 {
-id temp, list, tempCell;
-int index;
+    id temp, list, tempCell;
+    int index;
 
-	temp = NXGetNamedObject(@"prototypeManager", NSApp);
-	index = [[sender matrixInColumn:0] selectedRow];
-	[cell setLoaded:YES];
+    temp = NXGetNamedObject(@"prototypeManager", NSApp);
+    index = [[sender matrixInColumn:0] selectedRow];
+    [cell setLoaded:YES];
 
-	list = [temp equationList];
-	if (column == 0)
-	{
-		[cell setStringValue: [(ProtoEquation *)[list objectAtIndex:row] name]];
-		[cell setLeaf:NO];
-	}
-	else
-	{
-		tempCell = [[list objectAtIndex:index] objectAtIndex:row];
-		[cell setStringValue:[(ProtoEquation *)tempCell name]];
+    list = [temp equationList];
+    if (column == 0) {
+        [cell setStringValue:[(ProtoEquation *)[list objectAtIndex:row] name]];
+        [cell setLeaf:NO];
+    } else {
+        tempCell = [[list objectAtIndex:index] objectAtIndex:row];
+        [cell setStringValue:[(ProtoEquation *)tempCell name]];
 
-//		if ([[tempCell expression] maxPhone] >[currentPoint type])
-//			[cell setEnabled:NO];
-//		else
-//			[cell setEnabled:YES];
-		[cell setLeaf:YES];
-	}
+//        if ([[tempCell expression] maxPhone] >[currentPoint type])
+//            [cell setEnabled:NO];
+//        else
+//            [cell setEnabled:YES];
+        [cell setLeaf:YES];
+    }
 }
 
-- (void)setValue:sender
+- (void)setValue:(id)sender;
 {
-id temp = NXGetNamedObject(@"transitionBuilder", NSApp);
+    id temp = NXGetNamedObject(@"transitionBuilder", NSApp);
 
-	[currentPoint setValue:[sender doubleValue]];
-	[temp display];
-	[NXGetNamedObject(@"specialTransitionBuilder", NSApp) display]; 
+    [currentPoint setValue:[sender doubleValue]];
+    [temp display];
+    [NXGetNamedObject(@"specialTransitionBuilder", NSApp) display];
 }
 
-- (void)setType1:sender
+- (void)setType1:(id)sender;
 {
-id temp = NXGetNamedObject(@"transitionBuilder", NSApp);
+    id temp = NXGetNamedObject(@"transitionBuilder", NSApp);
 
-	[type2Button setState:0];
-	[type3Button setState:0];
-	[currentPoint setType:DIPHONE];
-	[temp display];
-	[NXGetNamedObject(@"specialTransitionBuilder", NSApp) display]; 
+    [type2Button setState:0];
+    [type3Button setState:0];
+    [currentPoint setType:DIPHONE];
+    [temp display];
+    [NXGetNamedObject(@"specialTransitionBuilder", NSApp) display];
 }
 
-- (void)setType2:sender
+- (void)setType2:(id)sender;
 {
-id temp = NXGetNamedObject(@"transitionBuilder", NSApp);
+    id temp = NXGetNamedObject(@"transitionBuilder", NSApp);
 
-	[type1Button setState:0];
-	[type3Button setState:0];
-	[currentPoint setType:TRIPHONE];
-	[temp display];
-	[NXGetNamedObject(@"specialTransitionBuilder", NSApp) display]; 
+    [type1Button setState:0];
+    [type3Button setState:0];
+    [currentPoint setType:TRIPHONE];
+    [temp display];
+    [NXGetNamedObject(@"specialTransitionBuilder", NSApp) display];
 }
 
-- (void)setType3:sender
+- (void)setType3:(id)sender;
 {
-id temp = NXGetNamedObject(@"transitionBuilder", NSApp);
+    id temp = NXGetNamedObject(@"transitionBuilder", NSApp);
 
-	[type1Button setState:0];
-	[type2Button setState:0];
-	[currentPoint setType:TETRAPHONE];
-	[temp display];
-	[NXGetNamedObject(@"specialTransitionBuilder", NSApp) display]; 
+    [type1Button setState:0];
+    [type2Button setState:0];
+    [currentPoint setType:TETRAPHONE];
+    [temp display];
+    [NXGetNamedObject(@"specialTransitionBuilder", NSApp) display];
 }
 
-- (void)setPhantom:sender
+- (void)setPhantom:(id)sender;
 {
-	[currentPoint setPhantom:[sender state]]; 
+    [currentPoint setPhantom:[sender state]];
 }
 
 @end
