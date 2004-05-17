d7752e39c2cb737ad51136b97d83271ec3a84e4f
Steve Nygard <nygard@gmail.com>
2004-05-17 01:38:59 +0000

    Put a copyright on all of my XML handling classes.

--- Applications/Monet/MXMLReferenceArrayDelegate.h
+++ Applications/Monet/MXMLReferenceArrayDelegate.h
@@ -2,8 +2,8 @@
 // $Id: MXMLReferenceArrayDelegate.h,v 1.1 2004/04/22 20:42:59 nygard Exp $
 //
 
-//  This file is part of __APPNAME__, __SHORT_DESCRIPTION__.
-//  Copyright (C) 2004 __OWNER__.  All rights reserved.
+//  This file is part of SNFoundation, a personal collection of Foundation extensions.
+//  Copyright (C) 2004 Steve Nygard.  All rights reserved.
 
 #import <Foundation/NSObject.h>
 
--- Applications/Monet/MXMLReferenceDictionaryDelegate.h
+++ Applications/Monet/MXMLReferenceDictionaryDelegate.h
@@ -2,8 +2,8 @@
 // $Id$
 //
 
-//  This file is part of __APPNAME__, __SHORT_DESCRIPTION__.
-//  Copyright (C) 2004 __OWNER__.  All rights reserved.
+//  This file is part of SNFoundation, a personal collection of Foundation extensions.
+//  Copyright (C) 2004 Steve Nygard.  All rights reserved.
 
 #import <Foundation/NSObject.h>
 
