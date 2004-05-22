c6677fd938ab826cd4fa6df7ce5014813f195f6b
Steve Nygard <nygard@gmail.com>
2004-05-22 21:56:53 +0000

    Added a couple scripts that I used to process the dictionary.  The perl script was my first attempt.  The python script was an experiment, and the one I used to clean up the dictionary.

---
+++ Scripts/dict.pl
@@ -0,0 +1,125 @@
+#!/usr/local/bin/perl
+
+use strict;
+use English;
+
+#use Cwd;
+#use POSIX qw(strftime);
+use Getopt::Std;
+use File::Basename;
+#use Data::Dumper;
+
+my %opts;
+my %keys;
+
+getopts('v', \%opts);
+
+if (scalar @ARGV < 1) {
+    my $short_program_name = basename($PROGRAM_NAME);
+
+    print "$short_program_name 1.0 (2004-04-03)\n";
+    print "Usage: $short_program_name <dictionary>\n\n";
+    exit 0;
+}
+
+my %dictionary;
+my %suffixes;
+my @suffix_order;
+
+&load_dictionary($ARGV[0]);
+&load_suffixes();
+
+#my $line = <STDIN>;
+#chomp($line);
+#$line = "The quick brown fox jumped over the lazy dog.";
+my $line = "The quick brown fox jumped over the lazy dog";
+print "line: '$line'\n";
+&process($line);
+
+sub load_dictionary {
+    my $filename = shift;
+    my $key;
+    my $value;
+
+    print "file: $filename\n";
+    open(MAINDICT, $filename) || die "Couldn't open file: $filename";
+    while (<MAINDICT>) {
+	chomp;
+	($key, $value) = split(/ /);
+	# Strip off part of speech information from key
+	$key =~ s|/([a-z]*)||g;
+	# Strip off trailing percent stuff from value -- I'm not sure if or how it is used.
+	$value =~ s/%.*//g;
+	#print "removed part of speech: $1\n";
+	if ($dictionary{$key} && $opts{'v'}) {
+	    print "Replacing value for $key, old value: $dictionary{$key}, new value: $value\n";
+	}
+	$dictionary{$key} = $value;
+    }
+    close(MAINDICT);
+}
+
+sub load_suffixes {
+    my $line;
+
+    open(SUFFIXES, "suffix_list.txt") || die "Couldn't open suffix_list.txt";
+    while ($line = <SUFFIXES>) {
+	chomp($line);
+#	print "line: $line\n";
+	if ($line =~ /^[a-z]/) {
+	    my ($suffix, $replacement, $tail_pronunciation) = split(/\t/, $line);
+#	    print "$suffix, $replacement, $tail_pronunciation\n";
+	    $suffixes{$suffix} = [$replacement, $tail_pronunciation];
+	    push(@suffix_order, $suffix);
+	}
+    }
+    close(SUFFIXES);
+}
+
+sub process {
+    my $line = shift;
+    my @words;
+    my $word;
+    my @prons;
+
+    @words = split(/ +/, $line);
+    foreach my $word (@words) {
+	my $pronunciation;
+
+	$word = lc $word;
+	$pronunciation = &lookup_word($word);
+#	print "word: '$word' -> $pronunciation\n";
+	push(@prons, $pronunciation);
+    }
+
+    print join(" ", @prons), "\n";
+}
+
+sub lookup_word {
+    my $word = shift;
+    my $pronunciation;
+    my $base_pron;
+
+    $pronunciation = $dictionary{$word};
+    if (!defined($pronunciation)) {
+#	print "Couldn't find pronunciation for $word, checking suffixes...\n";
+	for my $suffix (@suffix_order) {
+	    if ($word =~ /^(.+)$suffix$/) {
+		my $base = $1;
+		my $array_ref = $suffixes{$suffix};
+		my  ($replace, $tail) = @$array_ref;
+#		print "base: $base\n";
+#		print "suffix: $suffix, replace: '$replace', tail: '$tail'\n";
+		$base .= $replace;
+#		print "base now: $base\n";
+#		print "matched suffix $suffix, base: $base\n";
+		$base_pron = $dictionary{$base};
+		if (defined($base_pron)) {
+		    return $base_pron . $tail;
+		}
+	    }
+	}
+    }
+
+    $pronunciation;
+}
---
+++ Scripts/dict.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python
+
+import getopt, re, string, sys
+
+class Usage(Exception):
+      def __init__(self, msg):
+            self.msg = msg
+
+def usage():
+      print "dict.py 1.0 (2004-04-20)"
+      print "Usage: dict.py [-c] [-v] <dictionary>"
+      print
+
+info_map = {'a': 'Noun',
+            'b': 'Verb',
+            'c': 'Adjective',
+            'd': 'Adverb',
+            'e': 'Pronoun',
+            'f': 'Article',
+            'g': 'Preposition',
+            'h': 'Conjunction',
+            'i': 'Interjection',
+            'j': '???',
+            'k': 'Proper Name',
+            'l': 'Location Name',
+            'm': 'Concept Name',
+            }
+
+dict = {}
+
+def decode_info(info):
+      if not info: return ''
+      types = []
+      for c in info[:]:
+            try:
+                  types.append(info_map[c])
+            except:
+                  print "Unknown key", c, "in string", info
+      return string.join(types, ', ')
+
+def findword(w):
+      try:
+            pron = dict[w]
+      except KeyError:
+            pron = None
+      return pron
+
+def lookup():
+      print "> ",
+#      str = string.rstrip(sys.stdin.readline())
+      str = sys.stdin.readline().rstrip()
+      while str:
+            print "Str: ", str
+#            words = [string.lower(x) for x in re.split('\s+', str)]
+            words = [x.lower() for x in re.split('\s+', str)]
+#            prons = [dict[w] for w in words]
+#            prons = [findword(w) for w in words]
+            prons = []
+            for w in words:
+                  try:
+                        prons.append(dict[w])
+                  except:
+                        pass
+            print "Words:", words
+            print "Prons:", string.join(prons)
+            print "> ",
+            str = sys.stdin.readline().rstrip()
+
+def main(argv=None):
+      try:
+            _main(argv)
+      except Usage, err:
+            print >>sys.stderr, err.msg
+            print >>sys.stderr, "for help use --help"
+            return 2
+
+def _main(argv=None):
+      if argv == None:
+            argv = sys.argv
+
+      verbose = False
+      clean = False
+      test = False
+
+      try:
+            opts, args = getopt.getopt(argv[1:], "cvt")
+      except getopt.error, msg:
+            raise Usage(msg)
+
+      for o, a in opts:
+            if o == "-c":
+                  clean = True
+            if o == "-t":
+                  test = True
+            if o == "-v":
+                  verbose = True
+
+      f = open('2.0eMainDictionary', 'r')
+
+      copyright = f.readline();
+      if clean or verbose:
+            print copyright,
+
+      trans = string.maketrans(' ', '_')
+
+      line = f.readline()
+      while line:
+            line = line.rstrip()
+            key, value = re.split('\s+', line, 1)
+            m = re.match('(.+)/(.*)', key)
+            if m:
+                  word = m.group(1)
+                  pos = m.group(2)
+            else:
+                  word = key
+                  pos = None
+
+            value = string.translate(value, trans)
+            m = re.match('(.+)%(.+)', value)
+            if m:
+                  pron = m.group(1)
+                  info = m.group(2)
+            else:
+                  pron = value
+                  info = None
+
+            if verbose:
+                  if not pos: pos = '-'
+                  print "%-30s %-5s %-70s %s" % (word, pos, pron, decode_info(info))
+            elif clean:
+                  print key, value
+            elif test:
+                  dict[key] = pron
+
+            line = f.readline()
+
+      f.close()
+
+      if test:
+            lookup()
+
+if __name__ == '__main__':
+      sys.exit(main())
