ec8a38da207dc2f8987fead0be5715b81cf3edf3
Steve Nygard <nygard@gmail.com>
2004-08-05 00:08:01 +0000

    Slightly changes a bit of drawing.  The posture names are still being drawn in the wrong location.

--- Applications/Monet/EventListView.m
+++ Applications/Monet/EventListView.m
@@ -245,15 +245,12 @@
                 [[NSColor blackColor] set];
                 [[currentPhone symbol] drawAtPoint:NSMakePoint(currentX - 5.0, bounds.size.height - 42.0) withAttributes:nil];
             }
+        }
 
+        if (!mouseBeingDragged) {
             // TODO (2004-03-17): It still goes one pixel below where it should.
             [bezierPath moveToPoint:NSMakePoint(currentX + 0.5, bounds.size.height - (50.0 + 1 + (float)j * TRACKHEIGHT))];
             [bezierPath lineToPoint:NSMakePoint(currentX + 0.5, bounds.size.height - 50.0 - 1)];
-        } else {
-            if (!mouseBeingDragged) {
-                [bezierPath moveToPoint:NSMakePoint(currentX + 0.5, bounds.size.height - (50.0 + 1 + (float)j * TRACKHEIGHT))];
-                [bezierPath lineToPoint:NSMakePoint(currentX + 0.5, bounds.size.height - 50.0 - 1)];
-            }
         }
     }
     [[NSColor lightGrayColor] set];
