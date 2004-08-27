f630dbd9b92ce31e139d8d33fbc1fcb152b8e498
Steve Nygard <nygard@gmail.com>
2004-08-27 20:40:47 +0000

    Untabified file.  Moved function.  Added (commented out) debugging statements while I try to understand how this works.

--- Tools/softwareTRM/fir.c
+++ Tools/softwareTRM/fir.c
@@ -25,6 +25,8 @@ TRMFIRFilter *TRMFIRFilterCreate(double beta, double gamma, double cutoff)
     int i, pointer, increment, numberCoefficients;
     double coefficient[LIMIT+1];
 
+    //printf("TRMFIRFilterCreate(beta=%g, gamma=%g, cutoff=%.10f\n", beta, gamma, cutoff);
+
     newFilter = (TRMFIRFilter *)malloc(sizeof(TRMFIRFilter));
     if (newFilter == NULL) {
         fprintf(stderr, "Couldn't malloc() FIRFilter.\n");
@@ -33,12 +35,20 @@ TRMFIRFilter *TRMFIRFilterCreate(double beta, double gamma, double cutoff)
 
     /*  DETERMINE IDEAL LOW PASS FILTER COEFFICIENTS  */
     maximallyFlat(beta, gamma, &numberCoefficients, coefficient);
+    //printf("numberCoefficients: %d\n", numberCoefficients);
+#if 0
+    for (i = 0; i < numberCoefficients; i++)
+        printf("coef[%2d]: %17.10f\n", i, coefficient[i]);
+#endif
+    //printf("----------------------------------------------------------------------\n");
 
     /*  TRIM LOW-VALUE COEFFICIENTS  */
     trim(cutoff, &numberCoefficients, coefficient);
+    //printf("trimmed numberCoefficients: %d\n", numberCoefficients);
 
     /*  DETERMINE THE NUMBER OF TAPS IN THE FILTER  */
     newFilter->numberTaps = (numberCoefficients * 2) - 1;
+    //printf("newFilter->numberTaps: %d\n", newFilter->numberTaps);
 
     /*  ALLOCATE MEMORY FOR DATA AND COEFFICIENTS  */
     newFilter->FIRData = (double *)calloc(newFilter->numberTaps, sizeof(double));
@@ -60,12 +70,13 @@ TRMFIRFilter *TRMFIRFilterCreate(double beta, double gamma, double cutoff)
     increment = -1;
     pointer = numberCoefficients;
     for (i = 0; i < newFilter->numberTaps; i++) {
-	newFilter->FIRCoef[i] = coefficient[pointer];
-	pointer += increment;
-	if (pointer <= 0) {
-	    pointer = 2;
-	    increment = 1;
-	}
+        newFilter->FIRCoef[i] = coefficient[pointer];
+        //printf("FIRCoef[%2d] = coef[%2d] = %17.10f\n", i, pointer, coefficient[pointer]);
+        pointer += increment;
+        if (pointer <= 0) {
+            pointer = 2;
+            increment = 1;
+        }
     }
 
     /*  SET POINTER TO FIRST ELEMENT  */
@@ -75,7 +86,7 @@ TRMFIRFilter *TRMFIRFilterCreate(double beta, double gamma, double cutoff)
     /*  PRINT OUT  */
     printf("\n");
     for (i = 0; i < newFilter->numberTaps; i++)
-	printf("FIRCoef[%-d] = %11.8f\n", i, newFilter->FIRCoef[i]);
+        printf("FIRCoef[%-d] = %11.8f\n", i, newFilter->FIRCoef[i]);
 #endif
 
     return newFilter;
@@ -99,12 +110,59 @@ void TRMFIRFilterFree(TRMFIRFilter *filter)
     free(filter);
 }
 
+/******************************************************************************
+*
+*       function:       FIRFilter
+*
+*       purpose:        Is the linear phase, lowpass FIR filter.
+*
+*       arguments:      input, needOutput
+*
+*       internal
+*       functions:      increment, decrement
+*
+*       library
+*       functions:      none
+*
+******************************************************************************/
+
+double FIRFilter(TRMFIRFilter *filter, double input, int needOutput)
+{
+    double output = 0.0;
+
+    /*  PUT INPUT SAMPLE INTO DATA BUFFER  */
+    filter->FIRData[filter->FIRPtr] = input;
+    //printf("----------------------------------------------------------------------\n");
+    //printf("Added value at index %d, value: %17.10f, needOutput: %d\n", filter->FIRPtr, input, needOutput);
+
+    if (needOutput) {
+        int i;
+
+        /*  SUM THE OUTPUT FROM ALL FILTER TAPS  */
+        for (i = 0; i < filter->numberTaps; i++) {
+            //printf("output += FIRData[%d] * FIRCoef[%d]\n", filter->FIRPtr, i);
+            output += filter->FIRData[filter->FIRPtr] * filter->FIRCoef[i];
+            filter->FIRPtr = (filter->FIRPtr + 1) % filter->numberTaps;
+        }
+    }
+
+    /*  ADJUST THE DATA POINTER, READY FOR NEXT CALL  */
+    filter->FIRPtr = decrement(filter->FIRPtr, filter->numberTaps);
+    //printf("FIRPtr index is now %d\n", filter->FIRPtr);
+
+    //printf("FIRFilter(%g, %d) = %g\n", input, needOutput, 0.0);
+    return output;
+}
+
+//
+// Local functions
+//
 
 /******************************************************************************
 *
-*	function:	maximallyFlat
+*       function:       maximallyFlat
 *
-*	purpose:	Calculates coefficients for a linear phase lowpass FIR
+*       purpose:        Calculates coefficients for a linear phase lowpass FIR
 *                       filter, with beta being the center frequency of the
 *                       transition band (as a fraction of the sampling
 *                       frequency), and gamme the width of the transition
@@ -112,11 +170,11 @@ void TRMFIRFilterFree(TRMFIRFilter *filter)
 *
 *       arguments:      beta, gamma, np, coefficient
 *
-*	internal
-*	functions:	rationalApproximation
+*       internal
+*       functions:      rationalApproximation
 *
-*	library
-*	functions:	cos, pow
+*       library
+*       functions:      cos, pow
 *
 ******************************************************************************/
 
@@ -131,18 +189,18 @@ int maximallyFlat(double beta, double gamma, int *np, double *coefficient)
 
     /*  CUT-OFF FREQUENCY MUST BE BETWEEN 0 HZ AND NYQUIST  */
     if ((beta <= 0.0) || (beta >= 0.5))
-	return BETA_OUT_OF_RANGE;
+        return BETA_OUT_OF_RANGE;
 
     /*  TRANSITION BAND MUST FIT WITH THE STOP BAND  */
     betaMinimum = ((2.0 * beta) < (1.0 - 2.0 * beta)) ? (2.0 * beta) :
-	(1.0 - 2.0 * beta);
+        (1.0 - 2.0 * beta);
     if ((gamma <= 0.0) || (gamma >= betaMinimum))
-	return GAMMA_OUT_OF_RANGE;
+        return GAMMA_OUT_OF_RANGE;
 
     /*  MAKE SURE TRANSITION BAND NOT TOO SMALL  */
     nt = (int)(1.0 / (4.0 * gamma * gamma));
     if (nt > 160)
-	return GAMMA_TOO_SMALL;
+        return GAMMA_TOO_SMALL;
 
     /*  CALCULATE THE RATIONAL APPROXIMATION TO THE CUT-OFF POINT  */
     ac = (1.0 + cos(TWO_PI * beta)) / 2.0;
@@ -151,7 +209,7 @@ int maximallyFlat(double beta, double gamma, int *np, double *coefficient)
     /*  CALCULATE FILTER ORDER  */
     n = (2 * (*np)) - 1;
     if (numerator == 0)
-	numerator = 1;
+        numerator = 1;
 
 
     /*  COMPUTE MAGNITUDE AT NP POINTS  */
@@ -159,40 +217,40 @@ int maximallyFlat(double beta, double gamma, int *np, double *coefficient)
     ll = nt - numerator;
 
     for (i = 2; i <= (*np); i++) {
-	int j;
-	double x, sum = 1.0, y;
-	c[i] = cos(TWO_PI * ((double)(i-1)/(double)n));
-	x = (1.0 - c[i]) / 2.0;
-	y = x;
+        int j;
+        double x, sum = 1.0, y;
+        c[i] = cos(TWO_PI * ((double)(i-1)/(double)n));
+        x = (1.0 - c[i]) / 2.0;
+        y = x;
 
-	if (numerator == nt)
-	    continue;
+        if (numerator == nt)
+            continue;
 
-	for (j = 1; j <= ll; j++) {
-	    double z = y;
-	    if (numerator != 1) {
-		int jj;
-		for (jj = 1; jj <= (numerator - 1); jj++)
-		    z *= 1.0 + ((double)j / (double)jj);
-	    }
-	    y *= x;
-	    sum += z;
-	}
-	a[i] = sum * pow((1.0 - x), numerator);
+        for (j = 1; j <= ll; j++) {
+            double z = y;
+            if (numerator != 1) {
+                int jj;
+                for (jj = 1; jj <= (numerator - 1); jj++)
+                    z *= 1.0 + ((double)j / (double)jj);
+            }
+            y *= x;
+            sum += z;
+        }
+        a[i] = sum * pow((1.0 - x), numerator);
     }
 
 
     /*  CALCULATE WEIGHTING COEFFICIENTS BY AN N-POINT IDFT  */
     for (i = 1; i <= (*np); i++) {
-	int j;
-	coefficient[i] = a[1] / 2.0;
-	for (j = 2; j <= (*np); j++) {
-	    int m = ((i - 1) * (j - 1)) % n;
-	    if (m > nt)
-		m = n - m;
-	    coefficient[i] += c[m+1] * a[j];
-	}
-	coefficient[i] *= 2.0 / (double)n;
+        int j;
+        coefficient[i] = a[1] / 2.0;
+        for (j = 2; j <= (*np); j++) {
+            int m = ((i - 1) * (j - 1)) % n;
+            if (m > nt)
+                m = n - m;
+            coefficient[i] += c[m+1] * a[j];
+        }
+        coefficient[i] *= 2.0 / (double)n;
     }
 
     return 0;
@@ -202,18 +260,18 @@ int maximallyFlat(double beta, double gamma, int *np, double *coefficient)
 
 /******************************************************************************
 *
-*	function:	trim
+*       function:       trim
 *
-*	purpose:	Trims the higher order coefficients of the FIR filter
+*       purpose:        Trims the higher order coefficients of the FIR filter
 *                       which fall below the cutoff value.
 *
 *       arguments:      cutoff, numberCoefficients, coefficient
 *
-*	internal
-*	functions:	none
+*       internal
+*       functions:      none
 *
-*	library
-*	functions:	fabs
+*       library
+*       functions:      fabs
 *
 ******************************************************************************/
 
@@ -221,11 +279,11 @@ void trim(double cutoff, int *numberCoefficients, double *coefficient)
 {
     int i;
 
-    for (i = (*numberCoefficients); i > 0; i--) {
-	if (fabs(coefficient[i]) >= fabs(cutoff)) {
-	    (*numberCoefficients) = i;
-	    return;
-	}
+    for (i = *numberCoefficients; i > 0; i--) {
+        if (fabs(coefficient[i]) >= fabs(cutoff)) {
+            *numberCoefficients = i; // TODO (2004-08-26): Shouldn't this really be i+1, so that it includes this coefficient?
+            return;
+        }
     }
 }
 
@@ -233,76 +291,25 @@ void trim(double cutoff, int *numberCoefficients, double *coefficient)
 
 /******************************************************************************
 *
-*	function:	FIRFilter
+*       function:       increment
 *
-*	purpose:	Is the linear phase, lowpass FIR filter.
-*
-*       arguments:      input, needOutput
-*
-*	internal
-*	functions:	increment, decrement
-*
-*	library
-*	functions:	none
-*
-******************************************************************************/
-
-double FIRFilter(TRMFIRFilter *filter, double input, int needOutput)
-{
-    if (needOutput) {
-	int i;
-	double output = 0.0;
-
-	/*  PUT INPUT SAMPLE INTO DATA BUFFER  */
-	filter->FIRData[filter->FIRPtr] = input;
-
-	/*  SUM THE OUTPUT FROM ALL FILTER TAPS  */
-	for (i = 0; i < filter->numberTaps; i++) {
-	    output += filter->FIRData[filter->FIRPtr] * filter->FIRCoef[i];
-	    filter->FIRPtr = increment(filter->FIRPtr, filter->numberTaps);
-	}
-
-	/*  DECREMENT THE DATA POINTER READY FOR NEXT CALL  */
-	filter->FIRPtr = decrement(filter->FIRPtr, filter->numberTaps);
-
-	/*  RETURN THE OUTPUT VALUE  */
-        //printf("FIRFilter(%g, %d) = %g\n", input, needOutput, output);
-	return output;
-    } else {
-	/*  PUT INPUT SAMPLE INTO DATA BUFFER  */
-	filter->FIRData[filter->FIRPtr] = input;
-
-	/*  ADJUST THE DATA POINTER, READY FOR NEXT CALL  */
-	filter->FIRPtr = decrement(filter->FIRPtr, filter->numberTaps);
-
-        //printf("FIRFilter(%g, %d) = %g\n", input, needOutput, 0.0);
-	return 0.0;
-    }
-}
-
-
-
-/******************************************************************************
-*
-*	function:	increment
-*
-*	purpose:	Increments the pointer to the circular FIR filter
+*       purpose:        Increments the pointer to the circular FIR filter
 *                       buffer, keeping it in the range 0 -> modulus-1.
 *
 *       arguments:      pointer, modulus
 *
-*	internal
-*	functions:	none
+*       internal
+*       functions:      none
 *
-*	library
-*	functions:	none
+*       library
+*       functions:      none
 *
 ******************************************************************************/
 
 int increment(int pointer, int modulus)
 {
     if (++pointer >= modulus)
-	return 0;
+        return 0;
 
     return pointer;
 }
@@ -310,25 +317,25 @@ int increment(int pointer, int modulus)
 
 /******************************************************************************
 *
-*	function:	decrement
+*       function:       decrement
 *
-*	purpose:	Decrements the pointer to the circular FIR filter
+*       purpose:        Decrements the pointer to the circular FIR filter
 *                       buffer, keeping it in the range 0 -> modulus-1.
 *
 *       arguments:      pointer, modulus
 *
-*	internal
-*	functions:	none
+*       internal
+*       functions:      none
 *
-*	library
-*	functions:	none
+*       library
+*       functions:      none
 *
 ******************************************************************************/
 
 int decrement(int pointer, int modulus)
 {
     if (--pointer < 0)
-	return modulus - 1;
+        return modulus - 1;
 
     return pointer;
 }
@@ -337,18 +344,18 @@ int decrement(int pointer, int modulus)
 
 /******************************************************************************
 *
-*	function:	rationalApproximation
+*       function:       rationalApproximation
 *
-*	purpose:	Calculates the best rational approximation to 'number',
+*       purpose:        Calculates the best rational approximation to 'number',
 *                       given the maximum 'order'.
 *
 *       arguments:      number, order, numerator, denominator
 *
-*	internal
-*	functions:	none
+*       internal
+*       functions:      none
 *
-*	library
-*	functions:	fabs
+*       library
+*       functions:      fabs
 *
 ******************************************************************************/
 
@@ -360,10 +367,10 @@ void rationalApproximation(double number, int *order, int *numerator, int *denom
 
     /*  RETURN IMMEDIATELY IF THE ORDER IS LESS THAN ONE  */
     if (*order <= 0) {
-	*numerator = 0;
-	*denominator = 0;
-	*order = -1;
-	return;
+        *numerator = 0;
+        *denominator = 0;
+        *order = -1;
+        return;
     }
 
     /*  FIND THE ABSOLUTE VALUE OF THE FRACTIONAL PART OF THE NUMBER  */
@@ -375,27 +382,27 @@ void rationalApproximation(double number, int *order, int *numerator, int *denom
 
     /*  FIND THE BEST DENOMINATOR VALUE  */
     for (i = (*order); i <= orderMaximum; i++) {
-	double ps = i * fractionalPart;
-	int ip = (int)(ps + 0.5);
-	double error = fabs( (ps - (double)ip)/(double)i );
-	if (error < minimumError) {
-	    minimumError = error;
-	    modulus = ip;
-	    *denominator = i;
-	}
+        double ps = i * fractionalPart;
+        int ip = (int)(ps + 0.5);
+        double error = fabs( (ps - (double)ip)/(double)i );
+        if (error < minimumError) {
+            minimumError = error;
+            modulus = ip;
+            *denominator = i;
+        }
     }
 
     /*  DETERMINE THE NUMERATOR VALUE, MAKING IT NEGATIVE IF NECESSARY  */
     *numerator = (int)fabs(number) * (*denominator) + modulus;
     if (number < 0)
-	*numerator *= (-1);
+        *numerator *= (-1);
 
     /*  SET THE ORDER  */
     *order = *denominator - 1;
 
     /*  RESET THE NUMERATOR AND DENOMINATOR IF THEY ARE EQUAL  */
     if (*numerator == *denominator) {
-	*denominator = orderMaximum;
-	*order = *numerator = *denominator - 1;
+        *denominator = orderMaximum;
+        *order = *numerator = *denominator - 1;
     }
 }
