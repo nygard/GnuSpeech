ed9ec55e47235fb4bdb444e6fa92e172d4948c51
Steve Nygard <nygard@gmail.com>
2004-08-27 20:22:41 +0000

    Experimenting with some more veclib functions.

--- Tools/softwareTRM/t1.c
+++ Tools/softwareTRM/t1.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+#import <vecLib/vecLib.h>
 
 #define BETA                      5.658        /*  kaiser window parameters  */
 #define IzeroEPSILON              1E-21
@@ -58,13 +60,21 @@ double myIzero(double x)
 int main(int argc, char *argv[])
 {
     unsigned int index;
+    vector float v[] = {{1.0, 2.0, 3.0, 4.0}, {5.0, 6.0, 7.0, 8.0}};
+    float w[] = {2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
 
     printf("Izero(BETA) = Izero(%g) = %g\n", BETA, Izero(BETA));
     printf("myIzero(%g) = %g\n", BETA, myIzero(BETA));
-    for (index = 0; index < 10000000; index++) {
+    printf("j0(%g) = %g\n", BETA, j0(BETA));
+    for (index = 0; index < 10000; index++) {
         //Izero(BETA);
         myIzero(BETA);
     }
+
+    printf("vSsum(v): %f\n", vSsum(4, v));
+    printf("vSasum(v): %f\n", vSasum(4, v));
+    printf("vSasum(v): %f\n", vSasum(8, v));
+    printf("vSasum(w): %f\n", vSasum(8, w));
 
     return 0;
 }
