2a724fef3ed0eea43e1ed465de20d48e488207ce
Steve Nygard <nygard@gmail.com>
2007-02-12 01:13:21 +0000

    Upgraded project to Xcode 2.4.1.  Fixed GSPronunciationDictionary so that it compiles if you don't have a prefix header set up.

--- Applications/GnuSpeech/GSPronunciationDictionary.h
+++ Applications/GnuSpeech/GSPronunciationDictionary.h
@@ -1,9 +1,7 @@
 //  This file is part of __APPNAME__, __SHORT_DESCRIPTION__.
 //  Copyright (C) 2004 __OWNER__.  All rights reserved.
 
-#import <Foundation/NSObject.h>
-
-@class NSMutableArray, NSMutableDictionary;
+#import <Foundation/Foundation.h>
 
 @interface GSPronunciationDictionary : NSObject
 {
