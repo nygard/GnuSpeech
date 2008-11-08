8c0e5acb8a023f952573f8d657eb1645c1d8a964
Dalmazio Brisinda <dbrisinda@gmail.com>
2008-11-08 10:18:12 +0000

    Temporary add.

---
+++ Applications/PrEditor/PrEntry.h
@@ -0,0 +1,17 @@
+//
+//  PrEntry.h
+//  PrEditor
+//
+//  Created by Dalmazio on 13/10/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PrEntry : NSObject {
+@public	
+	NSString* phone;
+	NSString* partsOfSpeech;
+}
+
+@end
---
+++ Applications/PrEditor/PrEntry.m
@@ -0,0 +1,13 @@
+//
+//  PrEntry.m
+//  PrEditor
+//
+//  Created by Dalmazio on 13/10/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "PrEntry.h"
+
+@implementation PrEntry
+
+@end
