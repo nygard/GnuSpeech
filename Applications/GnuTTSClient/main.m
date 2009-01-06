289b34b5bc3b360819c76ced68dfa25f9b6665a0
Dalmazio Brisinda <dbrisinda@gmail.com>
2009-01-06 04:32:17 +0000

    The initial import of a simple text-to-speech client showing how to invoke the text-to-speech server functionality programatically.

---
+++ Applications/GnuTTSClient/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TTSClient
+//
+//  Created by Dalmazio on 02/01/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
