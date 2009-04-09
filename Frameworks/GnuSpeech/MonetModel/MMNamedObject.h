bf50ffb31d9a81261a84b3dc0091328d5ddc2a0d
Dalmazio Brisinda <dbrisinda@gmail.com>
2009-04-09 22:35:54 +0000

    Added GNU header and copyright information to all application and framework source files.

--- Frameworks/GnuSpeech/MonetModel/MMNamedObject.h
+++ Frameworks/GnuSpeech/MonetModel/MMNamedObject.h
@@ -1,5 +1,32 @@
-//  This file is part of __APPNAME__, __SHORT_DESCRIPTION__.
-//  Copyright (C) 2004 __OWNER__.  All rights reserved.
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 1991-2009 David R. Hill, Leonard Manzara, Craig Schock
+//  
+//  Contributors: Steve Nygard
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+//  MMNamedObject.h
+//  GnuSpeech
+//
+//  Created by Steve Nygard in 2004.
+//
+//  Version: 0.8
+//
+////////////////////////////////////////////////////////////////////////////////
 
 #import "MMObject.h"
 #import <Foundation/NSXMLParser.h>
--- Services/GnuSpeechService/GnuSpeechService.h
+++ Services/GnuSpeechService/GnuSpeechService.h
@@ -1,10 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 1991-2009 David R. Hill, Leonard Manzara, Craig Schock
+//  
+//  Contributors: Dalmazio Brisinda
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
 //
 //  GnuSpeechService.h
 //  GnuSpeechService
 //
 //  Created by Dalmazio on 03/01/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
 //
+//  Version: 0.5
+//
+////////////////////////////////////////////////////////////////////////////////
 
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
