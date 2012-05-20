84e2ee7411f5c1e301e7c3894fe2300d3d405b33
Steve Nygard <nygard@gmail.com>
2012-05-19 22:06:37 -0600

    Updated copyrights.  Reduced to two lines.

--- Applications/Synthesizer/conversion.h
+++ Applications/Synthesizer/conversion.h
@@ -1,52 +1,5 @@
-/*******************************************************************************
- *
- *  Copyright 1991-2009 David R. Hill, Leonard Manzara, Craig Schock
- *
- *  Contributors: Adam Fedor, David Hill
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License     
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *******************************************************************************
- *
- *  conversion.h
- *  Synthesizer
- *
- *  Created by Adam Fedor on 1/18/2003.
- *
- *  Version: 0.7.4
- *
- ******************************************************************************/
-
-
-/*  REVISION INFORMATION  *****************************************************
-
-$Author: rao $
-$Date: 2002/03/21 16:49:54 $
-$Revision: 1.1 $
-$Source: /cvsroot/gnuspeech/gnuspeech/trillium/src/Synthesizer/conversion.h,v $
-$State: Exp $
-
-
-$Log: conversion.h,v $
-Revision 1.1  2002/03/21 16:49:54  rao
-Initial import.
-
- * Revision 1.1.1.1  1994/05/20  00:22:02  len
- * Initial archive of TRM interactive Synthesizer.
- *
-
-******************************************************************************/
+//  This file is part of Gnuspeech, an extensible, text-to-speech package, based on real-time, articulatory, speech-synthesis-by-rules. 
+//  Copyright 1991-2012 David R. Hill, Leonard Manzara, Craig Schock
 
 /*  GLOBAL FUNCTIONS *********************************************************/
 extern double frequency(double pitch);
