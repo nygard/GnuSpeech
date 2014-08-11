c7a26ccc26559c38925a9913adb3efeeee351e97
Steve Nygard <nygard@gmail.com>
2014-08-11 17:56:51 -0600

    Add a target so that I can test some of these functions individually, and watch them in the debugger.

---
+++ Frameworks/GnuSpeech/FunctionTester/main.m
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+
+#import <GnuSpeech/GnuSpeech.h>
+
+int main(int argc, const char *argv[])
+{
+    @autoreleasepool
+    {
+        NSLog(@"Hey");
+        init_parser_module();
+        char input[1000] = "one %tb two %te three";
+        char output[1000];
+        long outputLength = sizeof(output);
+        gs_pm_mark_modes(input, output, strlen(input), &outputLength);
+    }
+
+    return 0;
+}
