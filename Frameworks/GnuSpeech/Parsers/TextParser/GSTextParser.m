29f0e034c4eed98cfe8a4a8de1454d0113448d89
Steve Nygard <nygard@gmail.com>
2014-08-11 13:58:03 -0600

    Filter unprintable characters out of input string.

--- Frameworks/GnuSpeech/Parsers/TextParser/GSTextParser.m
+++ Frameworks/GnuSpeech/Parsers/TextParser/GSTextParser.m
@@ -6,6 +6,31 @@
 
 // <http://userguide.icu-project.org/strings/regexp>
 
+@interface NSString (GSExtensions)
+- (NSString *)stringByReplacingCharactersInSet:(NSCharacterSet *)set withString:(NSString *)str;
+@end
+
+@implementation NSString (GSExtensions)
+
+- (NSString *)stringByReplacingCharactersInSet:(NSCharacterSet *)set withString:(NSString *)str;
+{
+    NSScanner *scanner = [[NSScanner alloc] initWithString:self];
+    [scanner setCharactersToBeSkipped:[NSCharacterSet characterSetWithCharactersInString:@""]];
+    NSMutableString *result = [[NSMutableString alloc] init];
+    while (![scanner isAtEnd]) {
+        NSString *str;
+        if ([scanner scanUpToCharactersFromSet:set intoString:&str]) [result appendString:str];
+
+        if ([scanner scanCharactersFromSet:set intoString:&str]) {
+            // 2014-08-11: Strictly speaking, this method should append spaces of the same length, but one space should be good enough for my needs.
+            [result appendString:@" "];
+        }
+    }
+    return [result copy];
+}
+
+@end
+
 @implementation GSTextParser
 {
     GSPronunciationDictionary *_userDictionary;
@@ -82,6 +107,8 @@
 
 #pragma mark -
 
+// TODO: (2014-08-11) The regular expressions and character sets should not be created each time in this method.  Create them in init.
+
 /// Convert all non-printable characters (except escape character) to blanks.
 /// Also connect words hyphenated over a newline.
 - (NSString *)_conditionInputString:(NSString *)str;
@@ -90,14 +117,19 @@
     NSLog(@"str: '%@'", str);
     NSError *reError;
     NSRegularExpression *re_hyphenation = [[NSRegularExpression alloc] initWithPattern:@"([:alnum:])-[\t\v\f\r ]*\n[\t\v\f\r ]*" options:0 error:&reError];
-    NSLog(@"re_hyphenation: %@, error: %@", re_hyphenation, reError);
+    //NSLog(@"re_hyphenation: %@, error: %@", re_hyphenation, reError);
     NSString *s1 = [re_hyphenation stringByReplacingMatchesInString:str options:0 range:NSMakeRange(0, [str length]) withTemplate:@"$1"];
-    NSLog(@"s1: '%@'", s1);
+    //NSLog(@"s1: '%@'", s1);
+
+    NSMutableCharacterSet *printableAndEscape = [[NSCharacterSet alphanumericCharacterSet] mutableCopy];
+    [printableAndEscape formUnionWithCharacterSet:[NSCharacterSet punctuationCharacterSet]];
+    [printableAndEscape formUnionWithCharacterSet:[NSCharacterSet symbolCharacterSet]];
+    NSParameterAssert([self.escapeCharacter length] == 1);
+    [printableAndEscape addCharactersInString:self.escapeCharacter];
+    [printableAndEscape addCharactersInString:@" "];
 
     // Keep escape character and printable characters, change everything else to a space.
-    NSRegularExpression *re_printable = [[NSRegularExpression alloc] initWithPattern:@"[:^print:]" options:0 error:&reError];
-    NSLog(@"re_printable: %@, error: %@", re_printable, reError);
-    NSString *s2 = [re_printable stringByReplacingMatchesInString:s1 options:0 range:NSMakeRange(0, [s1 length]) withTemplate:@" "];
+    NSString *s2 = [s1 stringByReplacingCharactersInSet:[printableAndEscape invertedSet] withString:@" "];
 
     NSLog(@"<  %s", __PRETTY_FUNCTION__);
     return s2;
