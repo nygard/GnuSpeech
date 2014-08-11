0dee21e88d3bbff0f831fac1c77313a465f08f1c
Steve Nygard <nygard@gmail.com>
2014-08-11 02:05:51 -0600

    Reduce copyright spam.

--- Frameworks/GnuSpeech/Parsers/TextParser/streams.h
+++ Frameworks/GnuSpeech/Parsers/TextParser/streams.h
@@ -17,15 +17,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *******************************************************************************
- *
- *  streams.h
- *  GnuSpeech
- *
- *  Created by Dalmazio on 04/05/09.
- *
- *  Version: 0.9.1
- *
  ******************************************************************************/
 
 #import "NXStream.h"
