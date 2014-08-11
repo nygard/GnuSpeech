8ce6e9ddc21f91547693aadb48ca498e7c603601
Steve Nygard <nygard@gmail.com>
2014-08-11 12:50:35 -0600

    Add some tests.

---
+++ Frameworks/GnuSpeech/GnuSpeechTests/ConditionInputTests.m
@@ -0,0 +1,58 @@
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+#import "GSTextParser.h"
+#import "GSTextParser-Private.h"
+
+@interface ConditionInputTests : XCTestCase
+@end
+
+@implementation ConditionInputTests
+{
+    GSTextParser *_parser;
+}
+
+- (void)setUp;
+{
+    [super setUp];
+
+    _parser = [[GSTextParser alloc] init];
+}
+
+- (void)tearDown;
+{
+    _parser = nil;
+
+    [super tearDown];
+}
+
+- (void)testNoChanges;
+{
+    NSString *input = @"creativity";
+    NSString *output = [_parser _conditionInputString:input];
+    XCTAssertEqualObjects(output, @"creativity");
+}
+
+- (void)testConnectHyphenationAcrossNewline;
+{
+    NSString *input = @"creativ-   \n     ity";
+    NSString *output = [_parser _conditionInputString:input];
+    XCTAssertEqualObjects(output, @"creativity");
+}
+
+// \u2193 is a Unicode down arrow, which doesn't match the regex for now.  I wanted to use an unprintable character, like \u0007, but Clang generates an error for that.
+- (void)testUnprintableBreaksHyphenationNewlinePattern;
+{
+    NSString *input = @"creativ- \u2193 \n ity";
+    NSString *output = [_parser _conditionInputString:input];
+    XCTAssertEqualObjects(output, @"creativ- \u2193 \n ity");
+}
+
+- (void)testMidHyphenationUntouched;
+{
+    NSString *input = @"creativ-ity";
+    NSString *output = [_parser _conditionInputString:input];
+    XCTAssertEqualObjects(output, @"creativ-ity");
+}
+
+@end
