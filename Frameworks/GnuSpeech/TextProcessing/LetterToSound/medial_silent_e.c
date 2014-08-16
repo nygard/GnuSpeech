0aa9efc5212319e4ed9da462c04ef7effc2c1524
Steve Nygard <nygard@gmail.com>
2014-08-16 17:52:39 -0600

    Reformat function.

--- Frameworks/GnuSpeech/TextProcessing/LetterToSound/medial_silent_e.c
+++ Frameworks/GnuSpeech/TextProcessing/LetterToSound/medial_silent_e.c
@@ -5,85 +5,79 @@
 
 void medial_silent_e(char *input, char **eow)
 {
-    char               *end = *eow;
-    register char      *position;
-    register int        index;
+    char *end = *eow;
 
-    for (position = input + 2; position < end - 5; position++) {
-        if (!member(position[0], "bcdfgmnprst"))
-            continue;               /* c */
-        if (!member(position[1], "bdfgkpt"))
-            continue;               /* k */
-        if ((position[2] != 'l') || (position[3] != 'e'))
-            continue;               /* le */
-        if (member(position[4] | 040, "aeiouy"))
-            continue;               /* s */
-        if (position[4] == '|')
-            continue;
+    for (char *position = input + 2; position < end - 5; position++)
+    {
+        if (!member(position[0],       "bcdfgmnprst")) continue; /* c */
+        if (!member(position[1],       "bdfgkpt"))     continue; /* k */
+        if (position[2] != 'l' || position[3] != 'e')  continue; /* le */
+        if ( member(position[4] | 040, "aeiouy"))      continue; /* s */
+        if (position[4] == '|')                        continue;
 
-        index = 5;
-        while (!member(position[index] | 040, "aeiouy|")) {     /* he */
+        int index = 5;
+        while (!member(position[index] | 040, "aeiouy|"))
+        {     /* he */
             index++;
-            if (&position[index] >= end) {
+            if (&position[index] >= end)
+            {
                 index = 0;
                 break;
             }
         }
 
-        if (!index)
-            continue;
-        if (position[index] == '|')
-            continue;
-        if ((position[index] == 'e') && (position[index + 1] == '|'))
-            continue;
+        if (!index)                                               continue;
+        if (position[index] == '|')                               continue;
+        if (position[index] == 'e' && position[index + 1] == '|') continue;
+
         insert_mark(&end, &position[3]);
         break;
     }
 
-    for (position = input; position < end - 5; position++) {
-        if ((member(position[0], "aeiou#")))
-            continue;
-        if (!member(position[1], "aiouy"))
-            continue;
-        if (member(position[2] | 040, "aehiouwxy"))
-            continue;
-        if (position[3] != 'e')
-            continue;
-        if (member(position[4] | 040, "aeiouynr"))
-            continue;
+    for (char *position = input; position < end - 5; position++)
+    {
+        if ( member(position[0],       "aeiou#"))    continue;
+        if (!member(position[1],       "aiouy"))     continue;
+        if ( member(position[2] | 040, "aehiouwxy")) continue;
+        if (position[3] != 'e')                      continue;
+        if ( member(position[4] | 040, "aeiouynr"))  continue;
 
-        index = 5;
-        if ((position[index] == '|') ||
-            ((position[index] == 'e') && (position[++index] == '|')))
+        int index = 5;
+        if (    position[index] == '|'
+            || (position[index] == 'e' && position[++index] == '|'))
+        {
             continue;
+        }
+
         index++;
-        if (!member(position[index] | 040, "aeiouy"))
-            continue;
+        if (!member(position[index] | 040, "aeiouy")) continue;
+
         insert_mark(&end, &position[3]);
         position[1] &= 0xdf;
         break;
     }
 
-    for (position = input + 1; position < end - 5; position++) {
-        if (position[0] != 'o')
+    for (char *position = input + 1; position < end - 5; position++)
+    {
+        if (position[0] != 'o')                      continue;
+        if (!member(position[1],       "aiouyU"))    continue;
+        if ( member(position[2] | 040, "aehiouwxy")) continue;
+        if (position[3] != 'e')                      continue;
+        if ( member(position[4] | 040, "aeiouynr"))  continue;
+
+        int index = 5;
+        if (    position[index] == '|'
+            || (position[index] == 'e' && position[++index] == '|'))
+        {
             continue;
-        if (!member(position[1], "aiouyU"))
-            continue;
-        if (member(position[2] | 040, "aehiouwxy"))
-            continue;
-        if (position[3] != 'e')
-            continue;
-        if (member(position[4] | 040, "aeiouynr"))
-            continue;
-        index = 5;
-        if ((position[index] == '|') ||
-            ((position[index] == 'e') && (position[++index] == '|')))
-            continue;
+        }
+
         index++;
-        if (!member(position[index] | 040, "aeiouy"))
-            continue;
+        if (!member(position[index] | 040, "aeiouy")) continue;
+
         insert_mark(&end, &position[3]);
         break;
     }
+
     *eow = end;
 }
