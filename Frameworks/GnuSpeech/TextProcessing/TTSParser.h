76f39b80d617a821cfbb6ef2bcd3ce513c567c30
Steve Nygard <nygard@gmail.com>
2014-08-15 23:31:27 -0600

    Add missing line back to copyright header.

--- Frameworks/GnuSpeech/TextProcessing/TTSParser.h
+++ Frameworks/GnuSpeech/TextProcessing/TTSParser.h
@@ -1,3 +1,4 @@
+//  This file is part of Gnuspeech, an extensible, text-to-speech package, based on real-time, articulatory, speech-synthesis-by-rules.
 //  Copyright 1991-2012 David R. Hill, Leonard Manzara, Craig Schock
 
 @class GSPronunciationDictionary;
