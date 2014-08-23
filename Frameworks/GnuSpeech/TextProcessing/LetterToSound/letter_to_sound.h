b9cff3ecc03b653ba7c4366544caa9447a15bc33
Steve Nygard <nygard@gmail.com>
2014-08-22 22:31:13 -0600

    Add documentation for parts of speech.

--- Frameworks/GnuSpeech/TextProcessing/LetterToSound/letter_to_sound.h
+++ Frameworks/GnuSpeech/TextProcessing/LetterToSound/letter_to_sound.h
@@ -2,11 +2,23 @@
 //  Copyright 1991-2012 David R. Hill, Leonard Manzara, Craig Schock
 
 /// Generate a pronunciation for a word using letter to sound rules.  The type of word
-/// is indicated after the pronunciation, separated by a percent.
+/// is indicated after the pronunciation, separated by a percent.  There may be multiple
+/// types, in which case the most common comes first.
 ///
-/// The possible types are: a, ab, ac, b, bc, c, ca, cb, d, j
-///
-/// The meaning of these types is not documented.
+/// The possible types are:
+///     NOUN                'a'
+///     VERB                'b'
+///     ADJECTIVE           'c'
+///     ADVERB              'd'
+///     PRONOUN             'e'
+///     ARTICLE             'f'
+///     PREPOSITION         'g'
+///     CONJUNCITON         'h'
+///     INTERJECTION        'i'
+///     UNKNOWN             'j'
+///     PROPERNAME (NOUN)   'k'
+///     LOCATIONNAME (NOUN) 'l'
+///     CONCEPTNAME (NOUN)  'm'
 ///
 /// @return Pronunciation of word, or NULL if any error (rare).
 char *letter_to_sound(char *word);
