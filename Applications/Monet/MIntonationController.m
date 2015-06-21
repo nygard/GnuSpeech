cfe9f7674bc64e1e768cbac3f914588664e499f7
Steve Nygard <nygard@gmail.com>
2015-06-20 21:04:21 -0600

    Copy the old window, and add some outlets.

--- Applications/Monet/MIntonationController.m
+++ Applications/Monet/MIntonationController.m
@@ -3,11 +3,24 @@
 
 #import "MIntonationController.h"
 
+#import "MAIntonationView.h"
+
 @interface MIntonationController ()
+@property (weak) IBOutlet MAIntonationView *intonationView;
+@property (weak) IBOutlet NSTextField *semitoneTextField;
+@property (weak) IBOutlet NSTextField *hertzTextField;
+@property (weak) IBOutlet NSTextField *slopeTextField;
+@property (weak) IBOutlet NSTableView *intonationRuleTableView;
+@property (weak) IBOutlet NSTextField *beatTextField;
+@property (weak) IBOutlet NSTextField *beatOffsetTextField;
+@property (weak) IBOutlet NSTextField *absoluteTimeTextField;
 
 @end
 
 @implementation MIntonationController
+{
+//    NSPrintInfo *_intonationPrintInfo;
+}
 
 - (id)init;
 {
