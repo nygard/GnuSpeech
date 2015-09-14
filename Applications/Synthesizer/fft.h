885f18360e250a089b0d08590212c90d5974d5b0
Steve Nygard <nygard@gmail.com>
2015-09-14 16:23:38 -0600

    Remove Synthesizer app.  This will be replaced by TRAcT.

--- Applications/Synthesizer/fft.h
+++
@@ -1,5 +0,0 @@
-//  This file is part of Gnuspeech, an extensible, text-to-speech package, based on real-time, articulatory, speech-synthesis-by-rules. 
-//  Copyright 1991-2012 David R. Hill, Leonard Manzara, Craig Schock
-
-void realfft(float *table, int tablesize);
-void four1(float *data, int nn, int isign);
